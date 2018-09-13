413963093b0f67ea83e869b12f2c03d3a6a30673
Aseem Rastogi <aseemr@microsoft.com>
2018-09-13 16:19:38 +0530

    updating mitls and F* hashes

--- hashes.sh
+++ hashes.sh
@@ -1,12 +1,12 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=29cfe395207a1bfc4ec4e9fa633aca70d6e1708e
+hashes[mitls-fstar]=3514764ba9dff895d94fe13fd4033099ef9e84d6
 branches[mitls-fstar]=master
 hashes[quackyducky]=6c15bdbbc63046e919c3c3ba822d8f6ae62078bb
 branches[quackyducky]=master
 hashes[vale]=27dc638909758deef6b6f44bd7f8ac4367c62180
 branches[vale]=master
-hashes[FStar]=72ce11d53c97f337088ffdaf8a67a31a6b6bd02e
+hashes[FStar]=8cf9e88506c5435741333afc8fb298ad457c0001
 branches[FStar]=master
 hashes[MLCrypto]=048fd71bf111e5020e6f47d884741d595cdb9ab3
 branches[MLCrypto]=master
