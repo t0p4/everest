f8f981e5c1f41cf07f109c2fa7d6091edd109098
Aseem Rastogi <aseemr@microsoft.com>
2018-09-18 13:40:37 +0530

    updating hashes

--- hashes.sh
+++ hashes.sh
@@ -1,16 +1,16 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=3514764ba9dff895d94fe13fd4033099ef9e84d6
+hashes[mitls-fstar]=3826e774fd2dd71c375f1ac05b1ab6705e1703b9
 branches[mitls-fstar]=master
 hashes[quackyducky]=6c15bdbbc63046e919c3c3ba822d8f6ae62078bb
 branches[quackyducky]=master
 hashes[vale]=27dc638909758deef6b6f44bd7f8ac4367c62180
 branches[vale]=master
-hashes[FStar]=8cf9e88506c5435741333afc8fb298ad457c0001
-branches[FStar]=master
+hashes[FStar]=32845c0465a6363782720c6ea4549866335b521c
+branches[FStar]=aseem_mm_monotonicity
 hashes[MLCrypto]=048fd71bf111e5020e6f47d884741d595cdb9ab3
 branches[MLCrypto]=master
-hashes[hacl-star]=40e6c26126fa0a66c3dc0ddcf67e3570d64811ae
+hashes[hacl-star]=e78b9241bb5f4236a1b8f0cdf9da1b159dbf285e
 branches[hacl-star]=fstar-master
-hashes[kremlin]=dce25162da8e4cc1082d19c2177b3003184e659b
+hashes[kremlin]=557abd04cd25b83549dd1311657edc7d080aaa48
 branches[kremlin]=master
