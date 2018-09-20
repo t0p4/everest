25ddc87659f8d3d61b7e05d7cb7390db56b85999
Aseem Rastogi <aseemr@microsoft.com>
2018-09-20 16:28:24 +0530

    bumping up caches

--- hashes.sh
+++ hashes.sh
@@ -1,16 +1,16 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=3826e774fd2dd71c375f1ac05b1ab6705e1703b9
+hashes[mitls-fstar]=fec0797cedcd837c27d34e1d314b4417a23e247f
 branches[mitls-fstar]=master
 hashes[quackyducky]=6c15bdbbc63046e919c3c3ba822d8f6ae62078bb
 branches[quackyducky]=master
 hashes[vale]=27dc638909758deef6b6f44bd7f8ac4367c62180
 branches[vale]=master
-hashes[FStar]=a9d75181b9b2988f1e2c0e3f71f1ccd7b0cf0bce
-branches[FStar]=aseem_mm_monotonicity
+hashes[FStar]=cd5db44c28a096316a9db9c3d735a9679580de3e
+branches[FStar]=aseem_cache_fix
 hashes[MLCrypto]=048fd71bf111e5020e6f47d884741d595cdb9ab3
 branches[MLCrypto]=master
-hashes[hacl-star]=e78b9241bb5f4236a1b8f0cdf9da1b159dbf285e
+hashes[hacl-star]=a3f0a6e429156e985bf16f5f7ef81dca04895a82
 branches[hacl-star]=fstar-master
-hashes[kremlin]=557abd04cd25b83549dd1311657edc7d080aaa48
+hashes[kremlin]=7fbfb47697af516fdd73f475b3b12843fbc3feb7
 branches[kremlin]=master
