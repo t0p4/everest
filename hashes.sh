729ef72ce2629585d255fee4259d85249087f20e
Aseem Rastogi <aseemr@microsoft.com>
2018-09-26 18:23:07 +0530

    upgrading hashes, trying out feq_sn

--- hashes.sh
+++ hashes.sh
@@ -1,16 +1,16 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=fec0797cedcd837c27d34e1d314b4417a23e247f
+hashes[mitls-fstar]=2d79fc614b901cc35012da8115e65565be759931
 branches[mitls-fstar]=master
 hashes[quackyducky]=6c15bdbbc63046e919c3c3ba822d8f6ae62078bb
 branches[quackyducky]=master
 hashes[vale]=0f0302411221d9f828a6f5851339d3a2a6bc3979
 branches[vale]=master
-hashes[FStar]=cd5db44c28a096316a9db9c3d735a9679580de3e
-branches[FStar]=aseem_cache_fix
+hashes[FStar]=804d807277bfece5bbe8cbaa9c72920cdcc33fbf
+branches[FStar]=feq_sn
 hashes[MLCrypto]=048fd71bf111e5020e6f47d884741d595cdb9ab3
 branches[MLCrypto]=master
-hashes[hacl-star]=a3f0a6e429156e985bf16f5f7ef81dca04895a82
+hashes[hacl-star]=cfaee74d93aabc2e6ee2149ca1644fa488f4dc63
 branches[hacl-star]=fstar-master
-hashes[kremlin]=7fbfb47697af516fdd73f475b3b12843fbc3feb7
+hashes[kremlin]=32ee4b8866c742876ebb5083c4c2b17cf056cbf6
 branches[kremlin]=master
